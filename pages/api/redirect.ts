354fa8afaa9acffefc6c1f1c0237388fadac33f7
Kalpesh Panchal <panchalkalpesh@gmail.com>
2021-04-15 23:27:53 -0400

    API redirect added

---
+++ pages/api/redirect.ts
@@ -0,0 +1,13 @@
+import { ObjectID } from "bson";
+import { connectToDatabase } from "./_connector";
+
+export default async (req, res) => {
+  const db = await connectToDatabase();
+  const entry = await db.db('links_db').collection('links_collection').findOne({
+    _id: new ObjectID(req.query.id as string)
+  });
+
+  if (entry) { return res.redirect(301, entry.link); }
+
+  return res.redirect(301, '/');
+}
